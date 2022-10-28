935447a5950b43f2a9dd98209e5502792c806c09
Francisco Timez <franciscotimez@gmail.com>
2022-10-28 18:57:23 -0300

    Add Ejercicio 2

--- ejercicios_genericos/Pulsadores/Ejercicio2/Ejercicio2.ino
+++ ejercicios_genericos/Pulsadores/Ejercicio2/Ejercicio2.ino
@@ -1,8 +1,27 @@
+// Consigna:
+// Conectar:
+// - Un pulsador con resistencia de pull_up.
+// - Un LED.
+
+// Leer el pulsador, con un pulso encender el LED y con otro pulso apargar el LED.
+
+// Definicion de pines
+#define PULSADOR1_PIN 7
+#define LED_AMARILLO_PIN 2
+
 void setup(){
   Serial.begin(115200);
-  Serial.println("Hola mundo");
+
+  // Inicializo pines
+  pinMode(PULSADOR1_PIN, INPUT);
+
+  pinMode(LED_AMARILLO_PIN, OUTPUT);
+  digitalWrite(LED_AMARILLO_PIN, LOW);
 }
 
 void loop(){
-  
+  if(!digitalRead(PULSADOR1_PIN)) {
+    digitalWrite(LED_AMARILLO_PIN, !digitalRead(LED_AMARILLO_PIN));
+  }
+  delay(200);
 }
